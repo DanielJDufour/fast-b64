a6aae054c41ac48824ec2b9f842d22f2f45bb9ce
DanielJDufour <daniel.j.dufour@gmail.com>
2020-12-09 13:10:18 -0500

    fixed bug

--- bits-to-base64.js
+++ bits-to-base64.js
@@ -1,13 +1,14 @@
 const bits2char = require('./bits-to-char.json');
 
-function bitstobase64 (bits) {
+function bitstobase64 (bits, debug) {
   var base64 = "";
 
   for (let i = 0; i < bits.length; i+=6) {
     base64 += bits2char[bits.substring(i, i + 6).padEnd(6, '0')];
   }
 
-  const padding = 4 - (base64.length % 4);
+  const padding = (3 - (base64.length % 3)) % 3;
+  if (debug) console.log("padding is:", padding);
   for (let i = 0; i < padding; i++) {
     base64 += "=";
   }
--- test.js
+++ test.js
@@ -1,9 +1,21 @@
+const { execSync } = require('child_process');
 const test = require('ava');
 const base64tobits = require('./base64-to-bits');
 const bitstobase64 = require('./bits-to-base64');
+// const arraybuffertobase64 = require('./arraybuffer-to-base64');
 
-const bits = '000011110000000000010111000000000001100000000000000010';
-const base64 = 'DwAXABgAC===';
+const bits = '000011110000000000010111000000000001100000000000000010000011';
+const base64 = 'DwAXABgACD==';
+
+const validate = b64 => {
+  try {
+    execSync('base64 --decode', { input: b64 });
+  } catch (error) {
+    console.error("b64 is invalid:", b64);
+    throw error;
+  }
+}
+validate(base64);
 
 test('converting from base64 to a string of bits', t => {
   t.is(base64tobits(base64), bits);
@@ -12,3 +24,15 @@ test('converting from base64 to a string of bits', t => {
 test('converting from bits to a base64 string', t => {
   t.is(bitstobase64(bits), base64);
 });
+
+/*
+test('converting array buffer to a base64 string', t => {
+  const nums = [];
+  for (let i = 0; i < 100; i++) nums.push(Math.random());
+  const float64array = new Float64Array(nums);
+  console.log({float64array});
+  const arrayBuffer = float64array.buffer;
+  console.log("arrayBuffer:", arrayBuffer);
+  t.is(arraybuffertobase64(arrayBuffer), base64);
+});
+*/
